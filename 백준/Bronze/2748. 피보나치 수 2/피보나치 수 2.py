0d267580f040e11f390666829ac732813d9df1b0
turt1e18 <52950852+turt1e18@users.noreply.github.com>
2025-04-14 23:24:13 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
@@ -3,12 +3,7 @@ from sys import stdin
 input = stdin.readline
 
 n = int(input().rstrip())
-temp, temp2 = 0, 1
 fibo = [0, 1]
-for _ in range(n - 1):
-    result = temp + temp2
-    fibo.append(result)
-    temp = temp2
-    temp2 = result
-
+for i in range(1, n):
+    fibo.append(fibo[i - 1]+fibo[i])
 print(fibo[-1])
